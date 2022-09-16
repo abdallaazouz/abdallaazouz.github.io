5ca4885e5a7323b1de366c6edc7e2d0cd89370e1
abdallaazouz <48604023+abdallaazouz@users.noreply.github.com>
2022-09-16 14:05:00 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,8 +27,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1ccbf36f633abc23976a66d7755b9a45",
-"/": "1ccbf36f633abc23976a66d7755b9a45",
+"index.html": "5395ecdc8a64667cb7eee8e8549f3636",
+"/": "5395ecdc8a64667cb7eee8e8549f3636",
 "main.dart.js": "707b5903c76c760aa87b259398aae102",
 "manifest.json": "1253291f986bd91bfcc9a2924adf42f7",
 "version.json": "13a8ca1868dd206a78baac84e18b4bf4"
